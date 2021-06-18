0701d187ac96887196d4605d776263601a484472
Myungjin <kjm1559@gmail.com>
2021-06-18 18:01:38 +0900

    bugfix

--- source/layers.py
+++ source/layers.py
@@ -15,48 +15,65 @@ class scaledDotProductAttentionLayer(tf.keras.layers.Layer):
     
 
 class multiHeadAttentionLayer(tf.keras.layers.Layer):
-    def __init__(self, head=4):
+    def __init__(self, d_model, head=4):
         super(multiHeadAttentionLayer, self).__init__()
         self.head = head
+        self.permute = Permute((2, 1, 3))
+        self.re1 = Reshape((-1, self.head, d_model//self.head))
+        self.re2 = Reshape((-1, d_model))
+                           
 
     def call(self, x, training):
         q, k, v = x
         # subspace header
-        q_s = Permute((2, 1, 3))(Reshape((-1, self.head, q.shape[-1]//self.head))(q))
-        k_s = Permute((2, 1, 3))(Reshape((-1, self.head, q.shape[-1]//self.head))(k))
-        v_s = Permute((2, 1, 3))(Reshape((-1, self.head, q.shape[-1]//self.head))(v))
+        q_s = self.permute(self.re1(q))
+        k_s = self.permute(self.re1(k))
+        v_s = self.permute(self.re1(v))
         
         # combine head
-        head = scaledDotProductAttentionLayer()([q_s, k_s, v_s])
-        head = Reshape((-1, q.shape[-1]))(Permute((2, 1, 3))(head))
+        head = scaledDotProductAttentionLayer()([q_s, k_s, v_s], training)
+        head = self.re2(self.permute(head))
         multi_head = linear(head)
         return multi_head
-        
 
-def mlp_layer(x, hidden_dim, output_dim):
-    x = Dense(hidden_dim, activation=gelu)(x)
-    output = Dense(output_dim)(x)
-    return output
+class mlpLayer(tf.keras.layers.Layer):
+    def __init__(self, hidden_dim, output_dim):
+        super(mlpLayer, self).__init__()
+        self.d1 = Dense(hidden_dim, activation=gelu)
+        self.d2 = Dense(output_dim)
+    def call(self, x, training):
+        x = self.d1(x)
+        return self.d2(x)
 
-        
-def transformerBlock(x, d_model, h_dim=128):
-    y = LayerNormalization()(x)
+class transformerBlock(tf.keras.layers.Layer):
+    def __init__(self, d_model, h_dim=128):
+        super(transformerBlock, self).__init__()
+        self.q_d = Dense(d_model)
+        self.k_d = Dense(d_model)
+        self.v_d = Dense(d_model)
+        self.ln1 = LayerNormalization()
+        self.ln2 = LayerNormalization()
+        self.mlp = mlpLayer(h_dim, d_model)
+        self.att = multiHeadAttentionLayer(d_model)
     
-    # multi head attention
-    q = Dense(d_model)(y) # query 
-    k = Dense(d_model)(y) # key
-    v = Dense(d_model)(y) # value
-    y = multiHeadAttentionLayer()([q, k, v])
-    
-    # skip connection
-    x = x + y
-    
-    # MLP layer
-    y = LayerNormalization()(x)
-    y = mlp_layer(y, h_dim, d_model)
-    
-    # skip connection
-    return x + y
+    def call(self, x, training):
+        y = self.ln1(x)
+
+        # multi head attention
+        q = self.q_d(y) # query 
+        k = self.k_d(y) # key
+        v = self.v_d(y) # value
+        y = self.att([q, k, v], training)
+
+        # skip connection
+        x = x + y
+
+        # MLP layer
+        y = self.ln2(x)
+        y = self.mlp(y, training)
+
+        # skip connection
+        return x + y        
 
     
 class visionTransformerLayer(tf.keras.layers.Layer):
@@ -67,7 +84,6 @@ class visionTransformerLayer(tf.keras.layers.Layer):
         self.patch_size = patch_size
         self.layer_num = layer_num
         
-    def build(self, input_shape):
         # learnabel class embedding
         self.class_emb = self.add_weight(shape=(1, 1, self.d_model),
                                         initializer='random_normal',
@@ -77,6 +93,9 @@ class visionTransformerLayer(tf.keras.layers.Layer):
         self.pos_emb = self.add_weight(shape=(1, self.num_patches + 1, self.d_model), 
                                       initializer='random_normal',
                                       trainable=True)
+        
+        self.dense = Dense(d_model)
+        self.t_layer = [transformerBlock(d_model) for i in range(layer_num)]
         
     def call(self, x, training):
         # feature extraction
@@ -90,7 +109,7 @@ class visionTransformerLayer(tf.keras.layers.Layer):
         )
         patches = Reshape((-1, self.patch_size**2 * x.shape[-1]))(patches)
         
-        x = Dense(self.d_model)(patches)
+        x = self.dense(patches)
         
         class_emb = tf.broadcast_to(self.class_emb, [batch_size, 1, self.d_model])
         x = tf.concat([class_emb, x], axis=1)
@@ -98,16 +117,18 @@ class visionTransformerLayer(tf.keras.layers.Layer):
         
         # transformer block
         for i in range(self.layer_num):
-            x = transformerBlock(x, self.d_model)
+            x = self.t_layer[i](x, training)
         
         return x
         
 
-def visionTransformer(input_dim, output_dim, image_size=32, patch_size=16):
+def visionTransformer(input_dim, output_dim, image_size=32, patch_size=4):
     inputs = tf.keras.Input(shape=input_dim)
     y = visionTransformerLayer(image_size, patch_size)(inputs)
+    print('y :', y.shape)
     y = Dense(output_dim, activation=gelu)(y[:, 0])
     outputs = Dense(output_dim, activation='softmax')(y)
+    print(outputs.shape)
     return tf.keras.Model(inputs, outputs, name='vit')
     
     
